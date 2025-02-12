7ff10b8baec08b9f771a6a4a1d12381eda0aa2b9
Adrienne Chiu <adrienne.chiu@fisherpaykel.com>
2025-02-13 10:14:54 +1300

    added running.py logic into the start of each cancel.py sceanario

--- fpabart_tests/tests/functions/test_cancel.py
+++ fpabart_tests/tests/functions/test_cancel.py
@@ -7,7 +7,50 @@ import random
 def test_cancel_active(appium_driver):
     """Cancel dry cycle when screen is active """
 
-    # Place in logic from running.py
+    #Check that I am on Home Screen
+    bart.step("Check for screen title")
+    dyer_title = appium_driver_helper.get_element(appium_driver, "title")
+
+    dyer_title_text = appium_driver_helper.get_text(appium_driver, dyer_title)
+    if dyer_title_text == "DRYER":
+        print("Home screen title verification passed.")
+    else:
+        print("Home screen title verification failed.")
+
+    # Check for material options and click one
+    bart.step("Check that all materials are present")
+    material_options =  ["Mixed", "Cotton", "Polyester", "Wool", "Silk", "Elastane", "Linen", "Down",] # Wool and cashmere does not have auto
+    sleep(1)
+    appium_driver_helper.swipe(appium_driver, "Down", "Mixed", 2)
+    material_options = ["Cashmere", "Acrylic", "Viscose", "Lyocell",  "Hemp", "Ramie", "Nylon"]
+    chosen_material = random.choice(material_options)
+
+    bart.step(f"Selecting random material: {chosen_material}")
+    material_element = appium_driver_helper.get_element(appium_driver, chosen_material)
+    material_element.click()
+    sleep(3)
+
+    # Verify that the time is set to "AUTO"
+    bart.step("Check if time is set to Auto")
+    Auto = appium_driver_helper.get_element(appium_driver, "Auto")
+    appium_driver_helper.get_text(appium_driver, Auto)
+
+    time_selection = appium_driver_helper.get_element(appium_driver, "time")
+    appium_driver_helper.get_text(appium_driver, time_selection)
+
+    #Click on Start button
+    bart.step("Click start")
+    start_btn = appium_driver_helper.get_element(appium_driver, "button-start") 
+    start_btn.click()
+
+    # Verify Captouch step
+    # bart.step("Use captouch to start and activate cycle")
+    
+    # Verify I am on Running Cycle Screen
+    bart.step("Check that I am on running cycle menu")
+    appium_driver_helper.get_element(appium_driver, "cycle-time-remaning")
+    appium_driver_helper.get_element(appium_driver, "button-cancel")
+    appium_driver_helper.get_element(appium_driver, "button-options")
     
     # Click on the cancel button
     bart.step("Click cancel button on active screen")
@@ -49,7 +92,50 @@ def test_cancel_active(appium_driver):
 def test_cancel_wake(appium_driver):
     """Cancel Dry cycle after waking up the screen"""
     
-    # Place in logic from running.py
+        #Check that I am on Home Screen
+    bart.step("Check for screen title")
+    dyer_title = appium_driver_helper.get_element(appium_driver, "title")
+
+    dyer_title_text = appium_driver_helper.get_text(appium_driver, dyer_title)
+    if dyer_title_text == "DRYER":
+        print("Home screen title verification passed.")
+    else:
+        print("Home screen title verification failed.")
+
+    # Check for material options and click one
+    bart.step("Check that all materials are present")
+    material_options =  ["Mixed", "Cotton", "Polyester", "Wool", "Silk", "Elastane", "Linen", "Down",] # Wool and cashmere does not have auto
+    sleep(1)
+    appium_driver_helper.swipe(appium_driver, "Down", "Mixed", 2)
+    material_options = ["Cashmere", "Acrylic", "Viscose", "Lyocell",  "Hemp", "Ramie", "Nylon"]
+    chosen_material = random.choice(material_options)
+
+    bart.step(f"Selecting random material: {chosen_material}")
+    material_element = appium_driver_helper.get_element(appium_driver, chosen_material)
+    material_element.click()
+    sleep(3)
+
+    # Verify that the time is set to "AUTO"
+    bart.step("Check if time is set to Auto")
+    Auto = appium_driver_helper.get_element(appium_driver, "Auto")
+    appium_driver_helper.get_text(appium_driver, Auto)
+
+    time_selection = appium_driver_helper.get_element(appium_driver, "time")
+    appium_driver_helper.get_text(appium_driver, time_selection)
+
+    #Click on Start button
+    bart.step("Click start")
+    start_btn = appium_driver_helper.get_element(appium_driver, "button-start") 
+    start_btn.click()
+
+    # Verify Captouch step
+    # bart.step("Use captouch to start and activate cycle")
+
+    # Verify I am on Running Cycle Screen
+    bart.step("Check that I am on running cycle menu")
+    appium_driver_helper.get_element(appium_driver, "cycle-time-remaning")
+    appium_driver_helper.get_element(appium_driver, "button-cancel")
+    appium_driver_helper.get_element(appium_driver, "button-options")
 
     # Wait for screen to go to sleep
     bart.step("Wait for screen to go to sleep")
@@ -89,7 +175,44 @@ def test_cancel_wake(appium_driver):
 def test_cancel_alert(appium_driver):
     """Clicking back button on the cancel cycle alert should not cancel cycle"""
 
-    # Place in logic from running.py
+    #Check that I am on Home Screen
+    bart.step("Check for screen title")
+    dyer_title = appium_driver_helper.get_element(appium_driver, "title")
+
+    dyer_title_text = appium_driver_helper.get_text(appium_driver, dyer_title)
+    if dyer_title_text == "DRYER":
+        print("Home screen title verification passed.")
+    else:
+        print("Home screen title verification failed.")
+
+    # Check for material options and click one
+    bart.step("Check that all materials are present")
+    material_options =  ["Mixed", "Cotton", "Polyester", "Wool", "Silk", "Elastane", "Linen", "Down",] # Wool and cashmere does not have auto
+    sleep(1)
+    appium_driver_helper.swipe(appium_driver, "Down", "Mixed", 2)
+    material_options = ["Cashmere", "Acrylic", "Viscose", "Lyocell",  "Hemp", "Ramie", "Nylon"]
+    chosen_material = random.choice(material_options)
+
+    bart.step(f"Selecting random material: {chosen_material}")
+    material_element = appium_driver_helper.get_element(appium_driver, chosen_material)
+    material_element.click()
+    sleep(3)
+
+    # Verify that the time is set to "AUTO"
+    bart.step("Check if time is set to Auto")
+    Auto = appium_driver_helper.get_element(appium_driver, "Auto")
+    appium_driver_helper.get_text(appium_driver, Auto)
+
+    time_selection = appium_driver_helper.get_element(appium_driver, "time")
+    appium_driver_helper.get_text(appium_driver, time_selection)
+
+    #Click on Start button
+    bart.step("Click start")
+    start_btn = appium_driver_helper.get_element(appium_driver, "button-start") 
+    start_btn.click()
+
+    # Verify Captouch step
+    # bart.step("Use captouch to start and activate cycle")
 
     # Verify I am on Running Cycle Screen
     bart.step("Check that I am on running cycle menu")
